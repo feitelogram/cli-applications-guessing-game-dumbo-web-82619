3e7663c99ebcb6f2e890bafbcae9a24a51299111
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-22 15:24:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   number = rand(6) + 1
   user_input = gets.chomp 
-  if user_input == number.join
+  if user_input == number.to_s
     puts "You guessed the correct number!"
   else if user_input == "exit"
     puts "Goodbye!"
